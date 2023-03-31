08cb2ae619bdae817fef45bf45df56c8c4003b53
Vishwas C P <129480620+vishwas-cp@users.noreply.github.com>
2023-03-31 21:40:38 +0530

    Add files via upload

---
+++ reactjs/calculator/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+export default function Footer() {
+    var stringCode = "</>"
+    return (
+        <footer>
+            <p> {stringCode} by: <a href="#">Vishwas Cp</a> </p>
+            <p><a rel="noreferrer" target="_blank" href="#">github</a></p>
+        </footer>
+    )
+}
