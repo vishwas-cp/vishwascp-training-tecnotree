305226d27343cda437ca84321df35b6d3230f355
Vishwas C P <129480620+vishwas-cp@users.noreply.github.com>
2023-03-31 20:27:56 +0530

    Add files via upload

---
+++ reactjs/webpage/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Loginform from './components/form'
+
+function App() {
+  return (
+    <div className="App">
+     <Loginform/>
+    </div>
+  );
+}
+
+export default App;
