ed4e5fa5b4c8ba0de7533a7b2f1800987f66426d
Vishwas C P <129480620+vishwas-cp@users.noreply.github.com>
2023-03-31 11:26:23 +0530

    Add files via upload

---
+++ Mini Web-based practice Projects using HTML-CSS-JavaScript/weather/app.js
@@ -0,0 +1,58 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+    // const API = `https://api.openweathermap.org/data/2.5/weather?
+    // q=${city}&appid=${API_KEY}&units=metric`
+    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+const getWeather = async(city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    return showWeather(data)
+}
+
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
+    weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
+
+
+
+
+(function($){
+    var search_button = $('.fa-search'),
+        close_button  = $('.close'),
+        input = $('.input');
+    search_button.on('click',function(){
+      $(this).parent().addClass('open');
+      close_button.fadeIn(500);
+      input.fadeIn(500);
+    });
+    
+    close_button.on('click',function(){
+      search_button.parent().removeClass('open');
+      close_button.fadeOut(500);
+      input.fadeOut(500);
+    });
+  })(jQuery);
