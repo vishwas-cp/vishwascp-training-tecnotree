1a7746300377a71fe7c516ffd488b132098da679
Vishwas C P <129480620+vishwas-cp@users.noreply.github.com>
2023-04-18 13:05:25 +0530

    Update p1.py

--- python/p1.py
+++ python/p1.py
@@ -1,4 +1,1 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-future_age = age + 10
-print("Hi " + name + "! In 10 years, you will be " + str(future_age) + " years old.")
+print("Hi In 10 years, you will be  years old.")
